e119cf113b9ad72808b67a6d99ed41db4810dfe2
unknown <arugayprincessangela@gmail.com>
2025-02-06 14:55:25 +0800

    Message

---
+++ src/app/page.js
@@ -0,0 +1,65 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex justify-center items-centers">
+      <img
+        src="/images/psee.jpg"
+        alt="psee"
+        className="w-[1100px] h-[500px] object-cover rounded-lg shadow-lg"
+      />
+    </div>
+
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black text-xl font-semibold bg-[#BCD2F5] bg-opacity-50 p-2 rounded">
+    <p>Elite Intelligence Bureau for Philippine</p>
+    <p>Power Players & Influential Players</p>
+    </div>
+
+
+    <div className="relative top-[50px] ">
+    <p>What is Entrepreneurship</p>
+    </div>
+
+    <div className="relative top-[50px] left-[30px]">
+    <p>Entrepreneurship is a process undertaken by an entrepreneur to augment his business interest. Thus Entrepreneurs do not emerge spontaneously on their own. Entrepreneurs appear to have been motivated by a combination and interaction of various factors. Thus entrepreneurship is influenced by a host of factors and in recent years entrepreneurship has become a fine art as it has evolved as a science of management and a catalytic tool of economic growth and development.</p>
+    <br></br>
+    <p>“Entrepreneurship is neither a science nor an art. It is a practice. It has a knowledge base. Knowledge in entrepreneurship is a means to an end. Indeed, what constitutes knowledge practice is largely defined by the ends, that is, by the practice.” – Peter Drucker</p>
+    </div>
+
+    <div className="relative top-[70px] left-[100px] -translate-x-[2px] -translate-y-[2px] text-black text-xl font-semibold bg-[#BCD2F5] bg-opacity-50 p-4 rounded max-w-[1000px] mx-4 my-2">
+    <p>Top 10 Successful CEOs</p>
+    <br></br>
+    <p>1. Patrick Perez (Inspire Next Global)</p>
+    <p>Inspire has been engaged with representatives of major companies and has been operating on small scale since 2012. During this period, activities were temporarily halted due to the pandemic, but we resumed operations in 2020. Following the restart, we experienced a surge in consultations from various companies. We have successfully facilitated the entry of Japanese companies into the market, acting as a bridge between Japanese and Philippine businesses. Our achievements have been favorable, and we plan to make a significant public presence in late 2022. We aim to go public within three years and continue our activities with the participation of all companies involved.</p>
+    <br></br>
+    <p>2. Henry Sy (Shoe Mart)</p>
+    <p>Shoe Mart, or SM for short, is one of the country’s retail behemoths, and far more than just a mall. It is now a well-loved institution of which many Filipino families hold fond memories. It provides a wide range of services, including shopping, theatres, locations for date nights, parties and other special occasions, ice skating, and food courts. SM started modestly and from humble beginnings, despite its now upmarket reputation.</p>   
+    <br></br>
+    <p>3. Tony Tan Caktiong (Jollibee Foods)</p>
+    <p>The cheerful tagline “langhap sarap” – a Jollibee trademark – is well-known among Filipinos. Tony Tan Caktiong, another Chinese immigrant, is the brains behind this popular food chain. His family maintained a Chinese restaurant in Manila at the time, which enabled him to finish college. He bought an ice cream shop in 1975, but owing to low sales, he decided to add other items such as fried chicken, fries, and burgers. Customers came to the store to buy his products after word spread in his area.</p>
+    <br></br>
+    <p>4. Socorro Ramos (National Book Store)</p>
+    <p>Socorro Ramos began her career in publishing and retail as a salesgirl in a bookshop. With a capital of PHP200, she and her husband launched National Book Store in Escolta at the age of 19, selling books and school supplies to children. The Japanese imposed censorship on books and publications at the time, which included her burgeoning business. She and her husband supplemented their income by selling other products such as candles and soaps.</p>
+    <br></br>
+    <p>5. John Gokongwei Jr. (J.G. Holdings)</p>
+    <p>Born in China, John Gokongwei Jr. was the heir to one of Cebu’s wealthiest families. When his father died, however, the family fortune evaporated and he had to support his family by peddling goods from his bicycle in the streets of Cebu. He then began to trade by boat and truck until he began importing from the United States.</p>
+    <br></br>
+    <p>6. Edgar Sia (Mang Inasal)</p>
+    <p>Edgar Sia, the brains behind Mang Inasal, is widely regarded as the Philippines’ pioneer in limitless rice meals. Sia, who is native to Iloilo City, dropped out of college at the age of 19 to start his own laundry and photo-developing business. Sia, then 26 years old, decided to launch the fast-food restaurant Mang Inasal – meaning “Mr Barbecue” in his native Hiligaynon – in 2003. In his hometown, the first branch was erected in a mall parking lot.</p>
+    <br></br>
+    <p>7. Mariano Que (Mercury Drug)</p>
+    <p>Following WWII, Mariano Que discovered a commercial opportunity. Que, who had worked at a local drugstore before the war, opened his own drugstore when he noticed the demand for sulfa, a type of antibiotic used to treat bacterial illnesses. He began selling good-quality sulfa for a low price. As a result, he was able to expand his clientele. In 1945, he founded the Mercury Drug chain of pharmacies.</p>
+    <br></br>
+    <p>8. Cresida Tueres (Greenwich Pizza)</p>
+    <p>Greenwich began as a modest over-the-counter pizza shop in Greenhills in 1971, founded by Cresida Tueres. Tueres had a natural talent for cooking; her friends and family were so taken with her food that they supported her in developing her humble shop into a national franchise. Jollibee Foods Corporation, impressed by Tueres’ commercial acumen and Greenwich’s expansion, struck an agreement in 1994 to buy 80 percent of Greenwich’s stock. The new company was renamed Greenwich Pizza Corporation after the successful commercial agreement, and it launched its first store in the Ever Gotesco Commonwealth Shopping Mall in 1994.</p>
+    <br></br>
+    <p>9. Cecilio Kwok Pedro (Lamoiyan Corporation)</p>
+    <p>The story of Cecilio Kwok Pedro is a classic example of Filipino entrepreneurial spirit overcoming the odds. Pedro was once the president of Aluminum Container Inc., a company that made aluminium toothpaste tubes for Procter & Gamble, the Philippine Refining Company (now Unilever), and Colgate-Palmolive. His clientele, however, switched to plastic-laminated toothpaste tubes due to environmental concerns. Aluminum Container Inc. went out of business in 1986.</p>
+    <br></br>
+    <p>10. Alfredo M Yao (Zest-O)</p>
+    <p>Alfredo Yao’s father died when he was 12; his mother supported him and his siblings by working as a street vendor. Yao was able to attend college at the Mapa Institute of Technology thanks to the help of a family member, but did not complete it. Yao did a variety of odd jobs, including working at a printing machine. He learned how to print cellophane wrapping for candy and biscuits during his time there, and was inspired to create his own printing company as a result of his experiences.</p>
+    </div>
+    </>
+  );
+}
